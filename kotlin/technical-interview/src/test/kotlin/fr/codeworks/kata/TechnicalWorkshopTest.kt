34eb308da5470c58563ce81cf5de3d7477cf8357
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-06-17 17:18:39 +0200

    feature: introduce industrialize solution to GM

--- kotlin/technical-interview/src/test/kotlin/fr/codeworks/kata/TechnicalWorkshopTest.kt
+++ kotlin/technical-interview/src/test/kotlin/fr/codeworks/kata/TechnicalWorkshopTest.kt
@@ -1,16 +1,8 @@
 package fr.codeworks.kata
 
-import jdk.internal.vm.vector.VectorSupport.test
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import java.io.BufferedReader
-import java.io.FileOutputStream
-import java.io.FileReader
-import java.io.PrintStream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.*
+import java.io.*
 
 internal class TechnicalWorkshopTest {
 
@@ -38,8 +30,35 @@ internal class TechnicalWorkshopTest {
 
         var line: String?
         while (gold.readLine().also { line = it } != null) {
-            Assertions.assertThat(line).isEqualTo(lead.readLine())
+            assertThat(line).isEqualTo(lead.readLine())
         }
-
     }
+
+    @Nested
+   inner class IndustrializedGoldenMaster{
+        private val baseFile = "src/test/resources/sample-data/base.txt"
+        private val baseFileArgumentsAsInput = "src/test/resources/sample-data/base_input.txt"
+        private val baseFileExpectedOutput = "src/test/resources/sample-data/base_expected_output.txt"
+
+        @Test
+        @DisplayName("Should dynamically replace the parameters in the base file and compare the result with the expected file content ")
+        fun shouldExtractContentDataFromSamples() {
+            val fileWithStaticContent = File(baseFile)
+            fileWithStaticContent.bufferedWriter().use { out ->
+                out.write("# a sample test data for testing purposes\n")
+                out.write("< some input (first line)\n")
+                out.write("> some matching output (first line)\n")
+                out.write("# some more comment\n")
+                out.write("< some more input (second line)\n")
+                out.write("> some more matching output (second line)\n")
+            }
+            TestExtractorHelper().extractContentOfFiles(baseFile)
+
+            val inputData = File(baseFileArgumentsAsInput).readText(Charsets.UTF_8)
+            assertThat(inputData).isEqualTo("some input (first line)\nsome more input (second line)")
+
+            val expectedOutputData = File(baseFileExpectedOutput).readText(Charsets.UTF_8)
+            assertThat(expectedOutputData).isEqualTo("some matching output (first line)\nsome more matching output (second line)")
+        }
+   }
 }
