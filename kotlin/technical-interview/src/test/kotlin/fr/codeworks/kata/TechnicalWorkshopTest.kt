16220adb7fe599eb850dc31fcae84665ab80d627
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-06-17 16:54:54 +0200

    adding test on static content

--- kotlin/technical-interview/src/test/kotlin/fr/codeworks/kata/TechnicalWorkshopTest.kt
+++ kotlin/technical-interview/src/test/kotlin/fr/codeworks/kata/TechnicalWorkshopTest.kt
@@ -1,13 +1,43 @@
 package fr.codeworks.kata
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.PrintStream
 
 internal class TechnicalWorkshopTest {
 
+    lateinit var out: PrintStream
+    private val filePath = "src/test/resources/init"
+
+    @BeforeEach
+    fun setup(){
+        out = System.out
+        System.setOut(PrintStream(FileOutputStream("$filePath/lead.txt")))
+
+    }
+    @AfterEach
+    fun destroy(){
+        System.setOut(out)
+    }
+
     @Test
-    fun shouldWork() {
-        val expected = 42
-        Assertions.assertThat(expected).isEqualTo(40+2)
+    @DisplayName("Should make sure the last outputs matches gold")
+    fun shouldRunMain() {
+        val gold = BufferedReader(FileReader("$filePath/gold.txt"))
+        val lead = BufferedReader(FileReader("$filePath/lead.txt"))
+
+        main()
+
+        var line: String?
+        while (gold.readLine().also { line = it } != null) {
+            Assertions.assertThat(line).isEqualTo(lead.readLine())
+        }
+
     }
 }
