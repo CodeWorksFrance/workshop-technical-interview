9a731ee5fdc4246f8895daf2ed6aac0f35829a70
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-06-20 16:12:05 +0200

    refacto: read base file instead

--- kotlin/technical-interview/src/test/kotlin/fr/codeworks/kata/TechnicalWorkshopTest.kt
+++ kotlin/technical-interview/src/test/kotlin/fr/codeworks/kata/TechnicalWorkshopTest.kt
@@ -9,56 +9,48 @@ internal class TechnicalWorkshopTest {
     lateinit var out: PrintStream
     private val filePath = "src/test/resources/init"
 
-    @BeforeEach
-    fun setup(){
-        out = System.out
-        System.setOut(PrintStream(FileOutputStream("$filePath/lead.txt")))
+    @Nested
+    inner class GoldenMaster {
+        @BeforeEach
+        fun setup() {
+            out = System.out
+            System.setOut(PrintStream(FileOutputStream("$filePath/lead.txt")))
 
-    }
-    @AfterEach
-    fun destroy(){
-        System.setOut(out)
-    }
+        }
 
-    @Test
-    @DisplayName("Should make sure the last outputs matches gold")
-    fun shouldRunMain() {
-        val gold = BufferedReader(FileReader("$filePath/gold.txt"))
-        val lead = BufferedReader(FileReader("$filePath/lead.txt"))
+        @AfterEach
+        fun destroy() {
+            System.setOut(out)
+        }
 
-        main()
+        @Test
+        @DisplayName("Should make sure the last outputs matches gold")
+        fun shouldRunMain() {
+            val gold = BufferedReader(FileReader("$filePath/gold.txt"))
+            val lead = BufferedReader(FileReader("$filePath/lead.txt"))
 
-        var line: String?
-        while (gold.readLine().also { line = it } != null) {
-            assertThat(line).isEqualTo(lead.readLine())
+            main()
+
+            var line: String?
+            while (gold.readLine().also { line = it } != null) {
+                assertThat(line).isEqualTo(lead.readLine())
+            }
         }
     }
 
     @Nested
-   inner class IndustrializedGoldenMaster{
-        private val baseFile = "src/test/resources/sample-data/base.txt"
-        private val baseFileArgumentsAsInput = "src/test/resources/sample-data/base_input.txt"
-        private val baseFileExpectedOutput = "src/test/resources/sample-data/base_expected_output.txt"
-
+    inner class IndustrializedGoldenMaster {
+        private val rootFolder = "src/test/resources/sample-data"
         @Test
         @DisplayName("Should dynamically replace the parameters in the base file and compare the result with the expected file content ")
-        fun shouldExtractContentDataFromSamples() {
-            val fileWithStaticContent = File(baseFile)
-            fileWithStaticContent.bufferedWriter().use { out ->
-                out.write("# a sample test data for testing purposes\n")
-                out.write("< some input (first line)\n")
-                out.write("> some matching output (first line)\n")
-                out.write("# some more comment\n")
-                out.write("< some more input (second line)\n")
-                out.write("> some more matching output (second line)\n")
-            }
-            TestExtractorHelper().extractContentOfFiles(baseFile)
+        fun shouldExtractContentDataFromSamplesAndTheOuputShouldMatch() {
+            TestExtractorHelper().extractContentOfFiles("$rootFolder/base.txt")
 
-            val inputData = File(baseFileArgumentsAsInput).readText(Charsets.UTF_8)
+            val inputData = File("$rootFolder/base_input.txt").readText(Charsets.UTF_8)
             assertThat(inputData).isEqualTo("some input (first line)\nsome more input (second line)")
 
-            val expectedOutputData = File(baseFileExpectedOutput).readText(Charsets.UTF_8)
+            val expectedOutputData = File("$rootFolder/base_expected_output.txt").readText(Charsets.UTF_8)
             assertThat(expectedOutputData).isEqualTo("some matching output (first line)\nsome more matching output (second line)")
         }
-   }
+    }
 }
