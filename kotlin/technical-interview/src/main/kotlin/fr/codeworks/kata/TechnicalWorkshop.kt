b865947b1dd699f7ea3d47cc29bceb2ec46130f5
Michelle AVOMO <michelle.avomo@codeworks.fr>
2022-06-20 16:18:54 +0200

    refacto: adding some characterization tests

--- kotlin/technical-interview/src/main/kotlin/fr/codeworks/kata/TechnicalWorkshop.kt
+++ kotlin/technical-interview/src/main/kotlin/fr/codeworks/kata/TechnicalWorkshop.kt
@@ -7,9 +7,9 @@ import java.io.File
 data class Question( val label: String, val answer: String,val difficulty: Int)
 data class CategorizedQuestions(val label: String, val questions: List<Question>)
 data class CandidateResponse(val response : String, val question: Question)
-data class Candidate(var firstname: String, var lastname: String, var email: String) {}
+data class Candidate(var firstname: String, var lastname: String, var email: String)
 
-class TechnicalWorkshop {
+open class TechnicalWorkshop {
     private var categories = mutableSetOf<String>()
     private var candidate: Candidate? = null
 
@@ -25,56 +25,77 @@ class TechnicalWorkshop {
 
     fun runCodeTest(c: String): Double {
         val resource = javaClass.getResource("/categories.json")
-        val file = File(resource.path)
+        val file = File(resource!!.path)
         val allCategoriesAsRaw = file.bufferedReader().use { it.readText() }
         val categoryType = object : TypeToken<List<CategorizedQuestions>>() {}.type
         val list = Gson().fromJson<List<CategorizedQuestions>?>(allCategoriesAsRaw, categoryType)
         val questions = list.find { q -> q.label == c }
-        val cq = questions
-        val question: List<Question>? = cq?.questions
+        val question: List<Question>? = questions?.questions
         val responses = mutableListOf<CandidateResponse>()
-        var s = 0.0 // s is for score
-        println("Welcome to the interview game. You'll have ${question?.size} questions on ${c}")
+        println("Welcome to the interview game. You'll have ${question?.size} questions on $c")
         print("Are you ready? (y) to start?\n")
-        val value = readLine() //response of the user
+        val value = readTheUserResponseForPlaying() //response of the user
         if (value == "y") {
             println("Let's go!")
             print("***************** Questions *****************\n")
-            question?.forEach { q ->
-                print(q.label)
-                val a = readLine()
-                if (a != null) {
-                    responses.add(CandidateResponse(a, q))
-                }
-            }
+            collectCandidatesAnswersToQuestions(question, responses)
             println("Thank you for your participation!")
         }
         println("***************** Response from: ${this.candidate?.firstname} *****************\n")
+        return computeScore(responses)
+    }
+
+    open fun computeScore(responses: MutableList<CandidateResponse>): Double {
+        var score = 0.0 // s is for score
         responses.forEach { candidateResponse ->
             val currentQuestion = candidateResponse.question
-            println("> Question: ${candidateResponse.question} ? \n")
-            println(">>> Response: ${candidateResponse.response}. \n")
+            println("> Question: ${candidateResponse.question} ?\n")
+            println(">>> Response: ${candidateResponse.response}.\n")
             print("----> What is your evaluation: t=true or f=false ?\n")
-            val answer = readLine()
+            val answer = readCodeworkerEvaluation()
             if (answer != null) {
                 if (answer == "t" || answer == "T") {
-                    when (currentQuestion.difficulty) {
-                        1 -> s += 0.25
-                        2 -> s += 0.5
-                        3 -> s += 0.75
-                        4 -> s += 1
+                    when (currentQuestion.difficulty ) {
+                        1 -> score += 0.25
+                        2 -> score += 0.5
+                        3 -> score += 0.75
+                        4 -> score += 1
                     }
                 }
             }
         }
-        return s
+        return score
     }
+
+    open fun collectCandidatesAnswersToQuestions(
+        question: List<Question>?,
+        responses: MutableList<CandidateResponse>
+    ) {
+        question?.forEach { q ->
+            print(q.label)
+            val a = readCandidateAnswersToQuestions()
+            print("\n")
+            if (a != null) {
+                responses.add(CandidateResponse(a, q))
+            }
+        }
+    }
+
+    open fun readTheUserResponseForPlaying() = readLine()
+
+    open fun readCodeworkerEvaluation() = readLine()
+
+    open fun readCandidateAnswersToQuestions() = readLine()
 
 }
 
 
 fun main() {
-    val codeTest = TechnicalWorkshop()
+    runTheWholeGame(TechnicalWorkshop())
+}
+
+fun runTheWholeGame(technicalWorkshop: TechnicalWorkshop) {
+    val codeTest = technicalWorkshop
     codeTest.addCat("SQL")
     codeTest.addCan("Toto", "Titi", "titi@mail.fr")
     val score = codeTest.runCodeTest("Java")
